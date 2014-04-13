130a124f9853b8b16de5ad7cf138a45fdfa63749
lhb-jr <lhb.jr@gmx.com>
2014-04-13 00:14:49 -0400

    moved to sys++

--- src/unitest.cxx
+++ src/unitest.cxx
@@ -1,12 +1,19 @@
 
-#define TEST_CLI test_cli
-
+#ifdef TEST_CLI
 #include "cli.hxx"
+#endif
+
+#ifdef TEST_LOG
+#include "log.hxx"
+int test_log(int argc, const char *argv[]);
+#endif
 
 int main(int argc, const char *argv[])
 {
 #ifdef TEST_CLI
 	return	TEST_CLI(argc, argv);
+#elif defined TEST_LOG
+	return TEST_LOG(argc,argv);
 #else
 	return 0;
 #endif
